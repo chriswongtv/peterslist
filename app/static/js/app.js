5fdcc4b1acb00f4b1580eece4c7adf4b713d27bb
chriswongtv <chris@cogifire.com>
2017-04-26 17:44:09 -0700

    Search icon event listener

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,12 +1,17 @@
 var main = new Vue({
 	el: '#main',
 	data: {
-
+		searchPanelType: ''
 	},
 	mounted: function() {
 
 	},
 	watch: {
 
+	},
+	methods: {
+		showSearchPanel: function(type) {
+			this.searchPanelType = type;
+		}
 	}
 });
