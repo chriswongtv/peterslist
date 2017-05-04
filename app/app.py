df5ab092c95e08e012dac86307340570286f782a
chriswongtv <chris@cogifire.com>
2017-05-03 23:56:22 -0700

    Added routes

--- app/app.py
+++ app/app.py
@@ -1,10 +1,24 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 app = Flask(__name__)
 
 @app.route("/")
-def main():
+def index():
 	return render_template('index.html')
+
+@app.route("/api/search")
+def search():
+	post_type = request.form['type']
+	search_keyword = request.form['keyword']
+	search_filters = request.form['filters']
+
+	# TODO: Perform search query and return result as JSON
+
+@app.route("<[post_type]>/<int:post_id>")
+def show_listing(post_type, post_id):
+	# TODO: Pull listing details from db and render the listing detail page
+	return render_template('listing.html')
 
 if __name__ == "__main__":
 	app.run()
