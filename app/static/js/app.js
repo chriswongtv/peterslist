77e6996b18f66b903e5b79988a0d3bcc03f690fd
chriswongtv <chris@cogifire.com>
2017-05-21 01:54:38 -0700

    Finalized event search options

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -7,9 +7,29 @@ var main = new Vue({
 		housingDateStart: new Date(),
 		housingOptions: ['Parking', 'Private bathroom', 'Pets allowed'],
 		housingCheckbox: [],
-		housingRoommate: '0',
+		housingSearchRoommate: '0',
 		housingPriceRange: [450, 1250],
 		eventSearchInput: '',
+		eventSearchCategories: ['All',
+								'Arts & Music',
+								'Camps, Trips & Retreats',
+								'Career',
+								'Charity & Causes',
+								'Class, Training & Workshops',
+								'Concerts & Performance',
+								'Conferences & Seminars',
+								'Festivals & Fairs',
+								'Food & Drink',
+								'Games & Competitions',
+								'Hobbies & Special Interest',
+								'Networking',
+								'Other',
+								'Parties & Social Gatherings',
+								'Religion & Spirituality',
+								'Science & Technology',
+								'Sports & Fitness',
+								'Travel & Outdoor'],
+		eventSearchCategory: 0,
 		eventSearchDate: '0'
 	},
 	delimiters: ['[[',']]'],
@@ -20,38 +40,7 @@ var main = new Vue({
 
 	},
 	computed: {
-		housingSearchRoommateLabel: function() {
-			switch (this.housingRoommate) {
-				case '0':
-					return 'All';
-				case '1':
-					return '1 (Single)';
-				case '2':
-					return '2 (Double)';
-				case '3':
-					return '3 (Triple)';
-				case '4':
-					return '4+';
-			}
-		},
-		eventSearchDateLabel: function() {
-			switch (this.eventSearchDate) {
-				case '0':
-					return 'All Dates';
-				case '1':
-					return 'Today';
-				case '2':
-					return 'Tomorrow';
-				case '3':
-					return 'This Week';
-				case '4':
-					return 'This Weekend';
-				case '5':
-					return 'Next Week';
-				case '6':
-					return 'Next Month';
-			}
-		}
+		
 	},
 	methods: {
 		showSearchPanel: function(type) {
@@ -60,14 +49,8 @@ var main = new Vue({
 		handleSearch: function() {
 			alert(this.searchInput);
 		},
-		setHousingSearchRoommate: function(option) {
-			this.housingRoommate = option;
-		},
-		setEventSearchDate: function(option) {
-			this.eventSearchDate = option;
-		},
 		handleEventSearch: function() {
-			
+
 		},
 		formatPriceRange: function(val) {
 			if (val == 1500)
