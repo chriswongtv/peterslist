11701cd5f363a20df95d848704d22c60e8edf897
chriswongtv <chris@cogifire.com>
2018-03-14 00:17:09 -0700

    Post listings

--- frontend/src/components/PostHousing.vue
+++ frontend/src/components/PostHousing.vue
@@ -63,7 +63,7 @@
           <el-input type="textarea" v-model="form.description"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">Post</el-button>
+          <el-button type="primary" @click="postHousing('Housing Lease')">Post</el-button>
         </el-form-item>
       </el-form>
       
@@ -109,7 +109,7 @@
           <el-input type="textarea" v-model="form.description"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="postHousing">Post</el-button>
+          <el-button type="primary" @click="postHousing('Housing Sale')">Post</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -131,6 +131,7 @@ export default {
         state: '',
         city: '',
         zip: '',
+        country: 'USA',
         price: '',
         homeType: '',
         dateAvailable: '',
@@ -145,7 +146,9 @@ export default {
     }
   },
   mounted: function() {
-
+    if (!this.isLoggedIn()) {
+      this.$router.push('/')
+    }
   },
   watch: {
 
@@ -154,8 +157,42 @@ export default {
     handleTabSelect: function(key) {
       this.index = parseInt(key);
     },
-    postHousing: function() {
+    postHousing: function(category) {
+      var formData = {
+        postInfo: {
+          location: {
+            buildingNumber: parseInt(this.form.buildingNumber),
+            streetName: this.form.streetName,
+            city: this.form.city,
+            state: this.form.state,
+            zip: parseInt(this.form.zip),
+            country: this.form.country
+          },
+          amount: parseFloat(this.form.price),
+          description: this.form.description,
+          createdOn: Date(),
+          userID: this.getUser(),
+          photos: ['https://images.unsplash.com/photo-1473447216727-44efba8cf0e0?dpr=1&auto=format&fit=crop&w=1500&h=1001&q=80&cs=tinysrgb&crop=&bg=']
+        },
+        dateAvailable: this.form.dateAvailable,
+        endDate: this.form.endDate,
+        homeType: this.form.homeType,
+        localityName: this.form.localityName,
+        postingCategory: category,
+        bedroomNumber: parseInt(this.form.bedroomNumber),
+        bathroomNumber: parseInt(this.form.bathroomNumber),
+        parkingNumber: parseInt(this.form.parkingNumber),
+        occupants: parseInt(this.form.occupants),
+        roommates: parseInt(this.form.roommates),
+        furnished: this.form.amenities.includes('Furnished'),
+        petAllowed: this.form.amenities.includes('Pets Allowed'),
+        hasParking: this.form.amenities.includes('Parking')
+      };
 
+      axios.post('http://cacofonix-1.ics.uci.edu:5000/api/post', formData)
+        .then((response) => {
+          console.log(response)
+        });
     }
   }
 }
